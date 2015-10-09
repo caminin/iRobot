e63bdc49ba437dbc7b6b6bdb0a96d739a6d3d985
flo5324 <flo.noel@hotmail.fr>
2015-10-09 12:13:53 +0200

    Correction base de travail

--- include/baseGeneration.hpp
+++ include/baseGeneration.hpp
@@ -1,15 +1,16 @@
-#include "../../../../../../usr/include/c++/5.1.1/iostream"
-#include "../../../../../../usr/include/c++/5.1.1/string"
-#include "../../../../../../usr/include/c++/5.1.1/fstream"
-#include "../../../../../../usr/include/c++/5.1.1/vector"
-#include "../../../../../../usr/include/c++/5.1.1/ostream"
-#include "../../../../../../usr/include/c++/5.1.1/algorithm"
+#include "ostream"
+#include "string"
+#include "fstream"
+#include "vector"
+#include "iostream"
+#include "algorithm"
 #include <string.h>
+using namespace std;
 
 class baseGeneration
 {
 	public :
-		bool myfind(std::vector<std::string> *myvector,std::string &ligne);
+		bool myfind(vector<std::string> *myvector,string &ligne);
 		void creerBaseFaitUnique();//go find baseDeConnaissance and make a baseFait
 		void afficheBaseFait();
 };
